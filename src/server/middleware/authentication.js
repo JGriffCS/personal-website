32c5c7442d3d0b96e64a60ecf8fe0f85336c7e70
Josh Griffiths <jgriffcs@gmail.com>
2018-05-14 00:40:30 +0900

    Correctly set auth header after login Update middleware to accept different token locations for fun

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -1,8 +1,20 @@
+const getToken = (req) => {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    return req.headers.authorization.split(' ')[1];
+  } else if (req.query && req.query.token) {
+    return req.query.token;
+  } else if (req.cookies && req.cookies.token) {
+    return req.cookies.token;
+  }
+
+  return null;
+}
+
 module.exports = (options = {}) => {
   const { jwt, secret } = options;
 
   return (req, res, next) => {
-    const token = (req.body ? req.body.token : null) || req.headers['token'];
+    const token = getToken(req);
 
     if (token) {
       jwt.verify(token, secret, (err, resp) => {
