18f4ec05ac99471af626f776fe9a497e49da4ecd
Josh Griffiths <jgriffcs@gmail.com>
2018-05-14 23:55:59 +0900

    Set token on app load (if present in local storage) Beginning redirect interceptor

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import axios from 'axios';
+import { createBrowserHistory } from 'history';
 
 import { blogPosts } from './app/reducers/blogPosts';
 
@@ -21,6 +24,17 @@ const createStoreWithMiddleware = compose(
 const store = createStoreWithMiddleware(reducers, initialState);
 
 import App from './app/App';
+
+// Configure axios
+axios.interceptors.response.use((resp) => {
+
+});
+
+// Set token if one is found
+const token = localStorage.getItem('id_token');
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 ReactDOM.render(
   <Provider store={store}>
