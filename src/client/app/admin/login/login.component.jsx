f6208109de86d09b82bed7b19fcb8696d0da631e
Josh Griffiths <jgriffcs@gmail.com>
2018-05-16 01:56:39 +0900

    Login working as expected

--- src/client/app/admin/login/login.component.jsx
+++ src/client/app/admin/login/login.component.jsx
@@ -8,16 +8,47 @@ import { loginService } from '../../services/login.service';
 class Login extends React.Component {
   constructor (props) {
     super(props);
+
+    this.inputChange = this.inputChange.bind(this);
+    this.login = this.login.bind(this);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
 
-  componentDidMount () {
+  inputChange (e) {
+    const target = e.target;
 
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
+  login (e) {
+    e.preventDefault();
+
+    loginService.login(this.state.username, this.state.password).then((resp) => {
+      localStorage.setItem('id_token', resp.data.token);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${resp.data.token}`;
+      this.props.history.push('/admin');
+    }, err => console.log(err));
   }
 
   render () {
     return (
-      <div>
-        <input type="text" />
+      <div className="login-container">
+        <div className="login-header">
+          <h2>Admin Login</h2>
+        </div>
+        <div className="login-form">
+          <form onSubmit={this.login}>
+            <input name="username" type="text" value={this.state.username} onChange={this.inputChange} />
+            <input name="password" type="password" value={this.state.password} onChange={this.inputChange} />
+            <input type="submit" className="login-submit" value="Login" />
+          </form>
+        </div>
       </div>
     );
   }
