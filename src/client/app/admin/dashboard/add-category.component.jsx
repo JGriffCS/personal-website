af56bcecce96764ab3fb84efd63a0c02165544a3
Josh Griffiths <jgriffcs@gmail.com>
2018-05-28 02:13:42 +0900

    Field binding

--- src/client/app/admin/dashboard/add-category.component.jsx
+++ src/client/app/admin/dashboard/add-category.component.jsx
@@ -5,29 +5,43 @@ class AddCategoryModal extends React.Component {
   constructor (props) {
     super(props);
 
-    this.state = {
+    this.handleInputChange = this.handleInputChange.bind(this);
 
+    this.state = {
+      name: '',
+      value: '',
+      icon: ''
     };
   }
 
+  handleInputChange (event) {
+    console.log('hi!');
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render () {
+    console.log(this.state);
     return (
       <div className="add-category-container">
         <div className="add-category-form-container">
           <form>
             <label>Category Name:</label>
-            <input type="text" id="Name" />
+            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
             <label>Route Identifier:</label>
-            <input type="text" id="Name" />
+            <input type="text" name="value" value={this.state.value} onChange={this.handleInputChange} />
             <label>Icon (FA):</label>
-            <input type="text" id="Name" />
+            <input type="text" name="icon" value={this.state.icon} onChange={this.handleInputChange} />
           </form>
         </div>
         <div className="add-category-preview">
           <div className="category">
-            <div className="title">Test</div>
+            <div className="title">{this.state.name}</div>
             <div className="visual">
-              <i className={`fas fas-newspaper`}></i>
+              <i className={`fas ${this.state.icon}`}></i>
             </div>
           </div>
         </div>
