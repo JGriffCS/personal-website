29c3b4fdf994561cf600ac460bf863a20c203ba9
Josh Griffiths <jgriffcs@gmail.com>
2018-06-04 00:40:02 +0900

    Dashboard Category reducer

--- src/client/app/reducers/admin-dashboard-categories.js
+++ src/client/app/reducers/admin-dashboard-categories.js
@@ -5,5 +5,34 @@ import {
 } from '../constants/action-types';
 
 export const adminDashboardCategories = (state = [], action) => {
+  switch (action.type) {
+    // Can only init the categories if they haven't been so already
+    case INIT_ADMIN_DASHBOARD_CATEGORIES:
+      if (state.length === 0) {
+        return [...action.categories];
+      }
 
-}
+      return state;
+
+    case ADD_ADMIN_DASHBOARD_CATEGORY:
+      if (action.category) {
+        return [...state, action.category];
+      }
+
+      return state;
+
+    case REMOVE_ADMIN_DASHBOARD_CATEGORY:
+      const categoryIdx = state.findIndex((category) => category.id === action.categoryId);
+      if (categoryIdx !== -1) {
+        return [
+          ...state.slice(0, categoryIdx),
+          ...state.slice(categoryIdx + 1),
+        ];
+      }
+
+      return state;
+
+    default:
+      return state;
+  }
+};
--- src/client/app/reducers/blogPosts.js
+++ src/client/app/reducers/blogPosts.js
@@ -1,11 +1,10 @@
 import { SET_BLOG_POSTS } from '../constants/action-types/blog.js';
 
 export const blogPosts = (state = [], action) => {
-  console.log(action);
   switch (action.type) {
     case SET_BLOG_POSTS:
       return [...state, ...action.posts];
     default:
       return state;
   }
-}
+};
