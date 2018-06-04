37f66217cc56d39b852e8b09fda388e1d4eff812
Josh Griffiths <jgriffcs@gmail.com>
2018-06-05 01:55:00 +0900

    Refactor dashboard category component to support the new remove functionality

--- src/client/app/admin/dashboard-category/dashboard-category.component.jsx
+++ src/client/app/admin/dashboard-category/dashboard-category.component.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const DashboardCategory  = (props) => {
-  return (
-    <Link to={`${props.match.url}/${props.category.value}`}>
-      <div className="category">
-        <div className="remove">
-          <i className="fas fa-trash-alt"></i>
+import { removeAdminDashboardCategory } from '../../actions/admin-dashboard-categories';
+
+class DashboardCategory extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.removeCategory = this.removeCategory.bind(this);
+  }
+
+  removeCategory (e) {
+    e.preventDefault();
+    console.log(this.props);
+  }
+
+  render () {
+    return (
+      <Link to={`${this.props.match.url}/${this.props.category.value}`}>
+        <div className="category">
+          <div className="remove" onClick={this.removeCategory}>
+            <i className="fas fa-trash-alt"></i>
+          </div>
+          <div className="title">{this.props.category.name}</div>
+          <div className="visual">
+            <i className={`fas ${this.props.category.icon}`}></i>
+          </div>
         </div>
-        <div className="title">{props.category.name}</div>
-        <div className="visual">
-          <i className={`fas ${props.category.icon}`}></i>
-        </div>
-      </div>
-    </Link>
-  );
-};
+      </Link>
+    );
+  }
+}
 
 DashboardCategory.propTypes = {
   category: PropTypes.shape({
@@ -29,4 +46,4 @@ DashboardCategory.propTypes = {
   }).isRequired
 };
 
-export default DashboardCategory;
+export default connect(null, dispatch => bindActionCreators({ removeAdminDashboardCategory }, dispatch))(DashboardCategory);
