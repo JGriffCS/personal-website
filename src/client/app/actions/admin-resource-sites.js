67b1aa200866d44b40944a6d824706853f5c34ec
Josh Griffiths <jgriffcs@gmail.com>
2018-06-17 02:45:00 +0900

    Action types and actions

---
+++ src/client/app/actions/admin-resource-sites.js
@@ -0,0 +1,30 @@
+import {
+  INIT_ADMIN_RESOURCE_SITES,
+  ADD_ADMIN_RESOURCE_SITE,
+  REMOVE_ADMIN_RESOURCE_SITE,
+} from '../constants/action-types';
+
+export function initAdminResourceSites(categoryId, sites) {
+  return {
+    type: INIT_ADMIN_RESOURCE_SITES,
+    categoryId,
+    sites,
+  };
+}
+
+export function addAdminResourceSite(categoryId, site) {
+  return {
+    type: ADD_ADMIN_RESOURCE_SITE,
+    categoryId,
+    site,
+  };
+}
+
+export function removeAdminResourceSite(categoryId, site) {
+  return {
+    type: REMOVE_ADMIN_RESOURCE_SITE,
+    categoryId,
+    site,
+  };
+}
+
