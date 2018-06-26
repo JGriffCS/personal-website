fd635f5ae24b46a15150dd76b75e97241d76620d
Josh Griffiths <jgriffcs@gmail.com>
2018-06-27 02:17:22 +0900

    Code is more verbose than necessary (I think). Committing just so I have history in case my theory is wrong. Improve mobile swiping logic.

--- src/client/app/admin/dashboard-category/dashboard-category.component.jsx
+++ src/client/app/admin/dashboard-category/dashboard-category.component.jsx
@@ -10,9 +10,15 @@ class DashboardCategory extends React.Component {
 
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.touchStart = this.touchStart.bind(this);
+    this.touchMove = this.touchMove.bind(this);
 
     this.state = {
+      lastTouchX: null,
+      lastTouchY: null,
       modalOpen: false,
+      showControls: false,
+      touchXDiff: 0,
     };
   }
 
@@ -30,6 +36,59 @@ class DashboardCategory extends React.Component {
     });
   }
 
+  touchStart(e) {
+    const touchObj = e.changedTouches[0];
+
+    this.setState({
+      touchXDiff: 0,
+      lastTouchX: touchObj.pageX,
+      lastTouchY: touchObj.pageY,
+    });
+  }
+
+  touchMove(e) {
+    const touchObj = e.changedTouches[0];
+
+    const diffX = touchObj.pageX - this.state.lastTouchX;
+    const diffY = touchObj.pageY - this.state.lastTouchY;
+
+    // Assume that if vertical movement greater than horizontal that not swiping horizontally
+    if (Math.abs(diffX) < Math.abs(diffY)) {
+      return;
+    }
+
+    let newControlState = null;
+    let newTouchDiff = null;
+    const totalDiff = this.state.touchXDiff + diffX;
+    
+    if (diffX < 0 && this.state.showControls) { // If swiping left and controls shown
+      if (totalDiff < -60) {
+        newControlState = false;
+        newTouchDiff = 0;
+      } else {
+        newTouchDiff = totalDiff;
+      }
+    } else if (diffX < 0 && !this.state.showControls) { // If swiping left and controls hidden
+      newTouchDiff = Math.max(totalDiff, 0);
+    } else if (diffX > 0 && this.state.showControls) { // If swiping right and controls shown
+      newTouchDiff = Math.min(totalDiff, 0);
+    } else if (diffX > 0 && !this.state.showControls) { // If swiping right and controls hidden
+      if (totalDiff > 60) {
+        newControlState = true;
+        newTouchDiff = 0;
+      } else {
+        newTouchDiff = totalDiff;
+      }
+    }
+
+    this.setState({
+      lastTouchX: touchObj.pageX,
+      lastTouchY: touchObj.pageY,
+      showControls: newControlState !== null ? newControlState : this.state.showControls,
+      touchXDiff: newTouchDiff !== null ? newTouchDiff : this.state.touchXDiff,
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -39,7 +98,14 @@ class DashboardCategory extends React.Component {
           close={this.closeModal}
         />
         <Link to={`${this.props.match.url}/${this.props.category.value}`}>
-          <div className="category">
+          <div className="dashboard-category-mobile-options">
+            Hi!
+          </div>
+          <div
+            className={`category ${this.state.showControls ? 'show-controls' : ''}`}
+            onTouchStart={this.touchStart}
+            onTouchMove={this.touchMove}
+          >
             <div className="remove" onClick={this.openModal}>
               <i className="fas fa-trash-alt" />
             </div>
