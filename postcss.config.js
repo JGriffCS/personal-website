6c795d04a068af1bf840c1de9543c192be2140c5
Josh Griffiths <jgriffcs@gmail.com>
2018-08-02 02:03:41 +0900

    Postcss mixins plugin Mixin for checkered pattern

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-mixins')({
+      mixins: require('./src/client/app/postcss-mixins.js'),
+    }),
     require('postcss-env-function')({
       variables: require('./src/client/app/postcss-variables.js'),
     }),
---
+++ src/client/app/postcss-mixins.js
@@ -0,0 +1,44 @@
+const variables = require('./postcss-variables');
+
+module.exports = {
+  // TODO: Accept hex colors as args to avoid base
+  checkered: (mixin, cols) => {
+    // If no columns passed or if the number of columns is odd, even/odd does the trick
+    if (!cols || cols % 2 !== 0) {
+      return {
+        '&:nth-child(odd)': {
+          'background-color': variables['--color-primary'],
+        },
+        '&:nth-child(even)': {
+          'background-color': variables['--color-alternate'],
+        },
+      };
+    }
+
+    // The nature of a checkered pattern requires a repeating pattern over 2 rows
+    const rows = 2;
+    const total = rows * cols;
+
+    // Case of nth-child(total * n) is difficult to accomplish in the loop
+    const mixinObj = {
+      [`&:nth-child(${total}n)`]: {
+        'background-color': variables['--color-primary'],
+      },
+    };
+
+    for (let i = 1; i < total; i++) {
+      let backgroundColor = variables['--color-primary'];
+
+      // Column colors of second row should be the reverse of first row
+      if ((i + Math.ceil(i / cols)) % 2 !== 0) {
+        backgroundColor = variables['--color-alternate'];
+      }
+
+      mixinObj[`&:nth-child(${total}n+${i})`] = {
+        'background-color': backgroundColor,
+      };
+    }
+
+    return mixinObj;
+  },
+};
