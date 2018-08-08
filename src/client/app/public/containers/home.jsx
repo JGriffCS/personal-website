6f467347cba03f29691772f01d1b80bf01e717fc
Josh Griffiths <jgriffcs@gmail.com>
2018-08-09 00:56:55 +0900

    Change home section headings

--- src/client/app/public/containers/home.jsx
+++ src/client/app/public/containers/home.jsx
@@ -17,14 +17,15 @@ function Home() {
             <img src={personalPhoto} alt="me" />
           </div>
           <div className="personal-description">
-            <h2>Josh Griffiths</h2>
+            <h3>Josh Griffiths</h3>
             <p>Etiam maximus blandit tincidunt. Ut nibh eros, gravida venenatis purus et, gravida blandit purus. Fusce et augue nec lorem pharetra lobortis vel vel mi. Vivamus dignissim finibus ex et consectetur. Donec rutrum sapien urna. Aenean ut varius lorem. Vestibulum ultrices nunc risus, vitae gravida dolor sodales sit amet. Sed eu risus est. Nulla neque nulla, dictum sed nulla ac, dictum dictum massa. Vestibulum suscipit non velit in lacinia. Duis blandit vehicula viverra. Duis eget elementum libero.</p>
           </div>
         </div>
+        <h3>Find Me</h3>
         <div className="contact-section-container">
-          <h2>Find Me</h2>
+
         </div>
-        <h2>Built Using</h2>
+        <h3>Built Using</h3>
         <div className="built-with-section-container">
           <img src={reactLogo} alt="React" />
           <img src={reduxLogo} alt="Redux" />
