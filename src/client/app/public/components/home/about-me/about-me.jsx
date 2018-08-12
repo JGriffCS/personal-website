091ba8366bec35c003870d6d250f279b1940b1f1
Josh Griffiths <jgriffcs@gmail.com>
2018-08-13 02:31:18 +0900

    Add social links

--- src/client/app/public/components/home/about-me/about-me.jsx
+++ src/client/app/public/components/home/about-me/about-me.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './about-me.pcss';
 import personalPhoto from '../../../../../assets/images/me.png';
+import github from '../../../../../assets/images/github.png';
+import linkedin from '../../../../../assets/images/linkedin.png';
+import gmail from '../../../../../assets/images/gmail.png';
 
 const AboutMe = () => (
   <div className="about-me-wrapper">
-    <img src={personalPhoto} alt="me" />
+    <img className="personal-photo" src={personalPhoto} alt="me" />
     <p>
       Software Engineer currently living in Tokyo, Japan. Roughly 5 years of full-stack web
       development experience with a specialization in front-end development. Currently working
@@ -18,6 +21,29 @@ const AboutMe = () => (
       hobby projects, playing video games, learning Japanese, and exploring around Tokyo. I&#39;m
       also very passionate about travel and try to do so as much as possible.
     </p>
+    <div className="social-links">
+      <a
+        href="mailto:jgriffcs@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={gmail} alt="gmail" />
+      </a>
+      <a
+        href="https://www.github.com/jgriffcs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} alt="github" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jgriffcs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={linkedin} alt="linkedin" />
+      </a>
+    </div>
   </div>
 );
 
