403b8f55bd282a32d1303c159565f9b0905acef0
Josh Griffiths <jgriffcs@gmail.com>
2018-08-18 18:45:25 +0900

    Remove unnecessary library

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const favicon = require('serve-favicon');
 const path = require('path');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
@@ -19,7 +18,6 @@ const pool = mysql.createPool({
   database: config.database.name,
 });
 
-app.use(favicon(path.join(__dirname, '..', 'dist', 'favicon.ico')));
 app.use('/api/admin/*', authentication({ jwt, secret: config.secret }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
