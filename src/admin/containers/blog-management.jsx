acbf3e4c9a7e5d488ab1ba9ba6aa10cd25e215d5
Josh Griffiths <jgriffcs@gmail.com>
2018-08-28 00:48:19 +0900

    Simplify blog management home page

--- src/admin/containers/blog-management.jsx
+++ src/admin/containers/blog-management.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import BlogManagementDashboard from '../components/blog-management/dashboard/dashboard';
+import DashboardItem from '../components/shared/dashboard-item/dashboard-item';
+import DashboardSection from '../components/shared/dashboard-section/dashboard-section';
 
 
 const BlogManagement = ({ match }) => (
   <div>
-    <Route
-      exact
-      path={match.path}
-      render={() => <BlogManagementDashboard match={match} />}
-    />
+    <Route exact path={match.path}>
+      <DashboardSection>
+        <DashboardItem path="add-post" icon="note_add" name="New Post" match={match} />
+        <DashboardItem path="edit-posts" icon="edit" name="Edit Posts" match={match} />
+        <DashboardItem path="add-tags" icon="playlist_add" name="Add Tags" match={match} />
+      </DashboardSection>
+    </Route>
   </div>
 );
 
